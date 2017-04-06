1ea486153c32eaf67c3347afb8680427939387a2
GeoSn0w <GeoSn0w@users.noreply.github.com>
2017-04-06 09:38:06 -0700

    This is the C source code of the crack-me tool.
    So that you can compile it for Linux or Windows.

---
+++ Level 2/tool.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+if(argc==2) {
+  printf("Preparing to check access key: %s\n", argv[1]);
+  int hex = 0;
+  for(int access=0; access<strlen(argv[1]); access++){
+	hex += (int)argv[1][access];
+  }
+  if(hex==1568) {
+    printf("Welcome! F.C.E. 365 Tool v35.1\n");
+    printf("What are you doing today?\n");
+    time_t timer;
+    char buffer[26];
+    struct tm* tm_info;
+    time(&timer);
+    tm_info = localtime(&timer);
+    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+    puts(buffer);
+    return 0;
+  }
+  else {
+    printf("The access key is not correct!\n");
+    printf("You are not allowed to use this software.\n");
+  }
+ } 
+else {
+   printf("To access the program you need the access key. Pass it with: <key>\n");
+}
+return 0;
+}
